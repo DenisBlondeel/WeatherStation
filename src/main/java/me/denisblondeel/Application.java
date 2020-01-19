88ab5063b3fa35f56262f1ba1f2306531df42b52
Denis Blondeel <denisblondeel@hotmail.com>
2020-01-19 16:35:12 +0100

    added amqp configuration file

--- src/main/java/me/denisblondeel/Application.java
+++ src/main/java/me/denisblondeel/Application.java
@@ -1,9 +1,7 @@
 package me.denisblondeel;
 
-import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
@@ -13,14 +11,6 @@ public class Application{
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    public Queue myQueue() {
-        return new Queue("weather_report", false);
-    }
-
-
-
 }
 
 
---
+++ src/main/java/me/denisblondeel/config/RabbitConfig.java
@@ -0,0 +1,29 @@
+package me.denisblondeel.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public Queue weatherReport() {
+        return new Queue("weatherReport", false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("amq.topic");
+    }
+
+    @Bean
+    Binding financeBinding(Queue weatherReport, TopicExchange exchange) {
+        return BindingBuilder.bind(weatherReport).to(exchange).with("#s");
+    }
+
+
+}
